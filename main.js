ee295fe1c83aaeceb5bbd40261c6419f7e542817
Stefan Omerovic <stefan.omerovic96@gmail.com>
2018-11-15 19:52:25 +0100

    Calculator End

--- main.js
+++ main.js
@@ -3,11 +3,12 @@ let viewer = document.querySelector('.viewer');
 const buttons = document.querySelectorAll('.button');
 const clearBtn = document.querySelector('.clear');
 const equalBtn = document.querySelector('.equal');
+const backBtn = document.querySelector('.back');
 
 // Insert Values Inside the Viewer
 function insertValues(value) {
   // First Prevents Addint Values That Should not be inside
-  if(value != 'C' && value != '=') {
+  if(value != 'C' && value != '=' && value != '<') {
     viewer.value += value;
   }
 }
@@ -15,6 +16,11 @@ function insertValues(value) {
 // Equal Function
 function equal() {
   viewer.value = eval(viewer.value);
+}
+
+function backward() {
+  let view = viewer.value;
+  viewer.value = view.substring(0, view.length -1);
 }
 
 // Clear Viewer Fully
@@ -25,6 +31,7 @@ function clear() {
 // Events
 clearBtn.addEventListener('click', clear);
 equalBtn.addEventListener('click', equal);
+backBtn.addEventListener('click', backward);
 
 buttons.forEach(button => button.addEventListener('click', function() {
   insertValues(button.value);
